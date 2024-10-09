f6a2c0f268a939816f055fbfd27b5aed91888270
fpereirasilv <fpereirasilv@gmail.com>
2024-10-09 16:35:16 -0300

    Metodo atacar concluído.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+class Heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome,
+        this.idade = idade,
+        this.tipo = tipo
+    }
+
+    atacar(){
+        let ataque;
+        switch (this.tipo.toLowerCase()) {
+        case 'mago':
+            ataque = 'magia';
+            break;
+        case 'guerreiro':
+            ataque = 'espada';
+            break;
+        case 'monge':
+            ataque = 'artes marciais';
+            break;
+        case 'ninja':
+            ataque = 'shuriken';
+            break;
+        default:
+            ataque = 'ataque desconhecido';
+        }
+    console.log(`O ${this.tipo} atacou usando ${ataque}.`);
+  }
+   
+}
+
+const heroi1 = new Heroi('Arthur', 28, 'guerreiro');
+heroi1.atacar();
